44623c6a1d34277977f67f3108331ab4214fa9fe
wklyu <wklyu@umich.edu>
2020-05-11 20:41:41 -0400

    create

---
+++ 2020.03.01/Expressions/Variables.py
@@ -0,0 +1,23 @@
+# test code
+x = 12
+y = 24
+print(x)
+x = 100
+x = 3.9 * x * (1 - x)
+z = x % 1000
+w = y ** 2
+print(x)
+print(z)
+print(w)
+ee = 'hello ' + 'world'
+a = type(ee)
+print(ee)
+print(a)
+print(float(99) + 100)
+nam = input('who are you? ')
+print('welcome', nam)
+
+# Convert elevator floors
+inp = input('Europe floor?')
+usf = int(inp) + 1
+print('US floor', usf)
